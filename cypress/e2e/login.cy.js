77a8bf6156127f731046de5d2eadc74a25d1afd7
Macieljs <jefley2016@gmail.com>
2025-02-26 14:22:51 -0300

    Implementando teste de Login

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,22 @@
+import dados from '../fixtures/dados_login.json'
+
+
+describe('Login - Validação do fluxo de autenticação', () => {
+    beforeEach(() => {
+        cy.visit("controller=authentication&back=my-account")
+    });
+    it('Deve permitir que um usuário válido faça login com sucesso', () => {
+      cy.login(dados.email,dados.senha)
+      cy.get('.page-heading').contains('My account')
+        
+    });
+    it('Deve aparecer a mensagem de falha na autenticação| e-mail errado', () => {
+        cy.login(dados.emailerrado,dados.senha)
+        cy.get('ol > li').contains('Authentication failed.')
+        
+    });
+    it('Deve aparecer a mensagem de falha na autenticação | senha errada', () => {
+        cy.login(dados.email,dados.senhaerrada)
+        cy.get('ol > li').contains('Authentication failed.')
+    });
+});
