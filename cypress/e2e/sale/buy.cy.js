f9c9aa056c3c82e6c56b9d8aac4c98eeb1cfdba3
Macieljs <jefley2016@gmail.com>
2025-03-14 10:59:48 -0300

    Salvando alterações antes de mudar de branch

--- cypress/e2e/sale/buy.cy.js
+++ cypress/e2e/sale/buy.cy.js
@@ -5,14 +5,12 @@ import salePage from '../../support/page-objects/sale.page';
 
 describe('Validando a Página de Compra - Login/Endereço Válidos', () => {
     beforeEach(() => {
-       
         cy.entrar()
-        cy.login(dados.email,dados.senha) 
-
+        cy.login(dados.email, dados.senha)
     });
-    
+
     it('Testando o fluxo de produtos para o carrinho', () => {
-        salePage.adicionandoProduto(5,1)
+        salePage.adicionandoProduto(2,1)
         cy.contains('Product successfully added to your shopping cart').should('be.visible')
     });
 });
--- cypress/support/page-objects/sale.page.js
+++ cypress/support/page-objects/sale.page.js
@@ -4,7 +4,6 @@ class SalePage {
     adicionandoProduto(index,color){
         cy.get('.sf-menu > :nth-child(1) > [href="http://www.automationpractice.pl/index.php?id_category=3&controller=category"]').click()
         cy.get(`:nth-child(${index}) > .product-container > .right-block > .button-container > .lnk_view > span`).click()
-        // cy.get(`#color_${color}`) //
         cy.get(`#color_to_pick_list > :nth-child(${color}`).click()
         cy.get('#quantity_wanted').type('5')
         cy.get('.exclusive > span').click()
